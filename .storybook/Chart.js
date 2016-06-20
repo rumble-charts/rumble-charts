5d3dfc4e6fb6e6da656fb6d962c66ae883fc5726
rosko <a@insvit.com>
2016-06-20 15:51:06 +0300

    add responsive example (#8)

--- .storybook/Chart.js
+++ .storybook/Chart.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import _ from 'lodash';
-import { storiesOf, action } from '@kadira/storybook';
-const {Chart, Bars, Lines} = require('../src');
+import { storiesOf } from '@kadira/storybook';
+const {Chart, Bars, Lines, Layer, Handlers, Animate, Dots, Ticks} = require('../src');
 const Filters = require('../docs/examples/filters');
 
 const series1 = [{
@@ -15,6 +14,48 @@ const series2 = [{
 }, {
     data: [13, 17, 19]
 }];
+
+const series3 = [{
+    data: [10, 11, 12, 13, 12, 12, 15, 16, 15, 17, 18, 20]
+}];
+
+let hovered = null;
+
+function hideHovered() {
+    if (hovered && hovered.circle) {
+        hovered.circle.setAttribute('r', hovered.radius);
+        hovered.circle.style.fillOpacity = hovered.opacity;
+        if (hovered.label) {
+            hovered.label.style.display = 'none';
+        }
+    }
+}
+
+function handleMouseMove({closestPoints}) {
+    const closest = closestPoints[0];
+    if (!closest) {
+        return;
+    }
+    const {seriesIndex, pointIndex} = closest;
+    const circle = document.querySelector(`circle.dots-circle-${seriesIndex}-${pointIndex}`);
+    if (!circle) {
+        return;
+    }
+    hideHovered();
+    const label = document.querySelector(`.labels-label-${seriesIndex}-${pointIndex}`);
+    hovered = {circle, label, radius: circle.getAttribute('r'), opacity: circle.style.fillOpacity};
+    circle.setAttribute('r', 5);
+    circle.style.fillOpacity = 1;
+    if (label) {
+        label.style.display = 'block';
+    }
+}
+
+function handleMouseLeave() {
+    hideHovered();
+}
+
+const months = ['JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC', 'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN']
 
 storiesOf('Chart', module)
     .add('default', () =>
@@ -33,4 +74,55 @@ storiesOf('Chart', module)
     .add('filters', () =>
         <Filters />
     )
-;
+    .add('responsive', () =>
+        <div style={{paddingLeft:'10em',marginTop:'5em'}}>
+            <svg viewBox='0 0 300 150'>
+                <Chart tag='g' width={300} height={150} series={series3}>
+                    <Layer width='100%' height='68%' position='middle center'>
+                        <Handlers onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} optimized={false}>
+                            <Bars
+                                colors={['#03a9f4']}
+                                groupPadding='1%'
+                                innerPadding='0%'
+                                barWidth='0%'
+                                barAttributes={{
+                                    strokeLinejoin: 'round',
+                                    strokeWidth: 21,
+                                    stroke: '#f5f5f6',
+                                    transform: 'translate(0 12)'
+                                }}
+                            />
+                            <Lines
+                                colors={['#007696']}
+                                lineWidth={0}
+                                interpolation='cardinal'
+                                lineAttributes={{
+                                    strokeLinecap: 'round',
+                                    strokeWidth: 5
+                                }}
+                            />
+                            <Dots
+                                className='dots'
+                                colors={['#007696']}
+                                dotStyle={{transition:'all 250ms',fillOpacity:0}}
+                            />
+                            <Ticks
+                                axis='x'
+                                ticks={months.map((label, x) => ({x, label}))}
+                                lineVisible={false}
+                                labelStyle={{
+                                    textAnchor:'middle',
+                                    dominantBaseline:'text-after-edge',
+                                    fill:'#000',
+                                    fontFamily: 'sans-serif',
+                                    fontWeight: 'normal',
+                                    fontSize: 10
+                                }}
+                                labelAttributes={{y: '2.5em'}}
+                            />
+                        </Handlers>
+                    </Layer>
+                </Chart>
+            </svg>
+        </div>
+    );
