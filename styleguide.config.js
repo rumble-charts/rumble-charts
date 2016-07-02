7fa249ceb1bc293ca4413d24a95290b2518febaf
rosko <a@insvit.com>
2016-07-03 02:06:21 +0300

    update docs

--- styleguide.config.js
+++ styleguide.config.js
@@ -3,7 +3,7 @@
 var path = require('path');
 
 module.exports = {
-    title: 'Rumble Charts 1.0.10',
+    title: 'Rumble Charts 1.0.11',
     sections: [{
         name: 'Rumble Charts', content: './docs/heading.md'
     }, {
